f0f1620f917f9af55759f95b95783309b8dcab2f
Fabrício Soares Oliveira <105008415+FabricioS0@users.noreply.github.com>
2023-10-23 16:37:57 -0300

    Atualização da visualização

--- DLL.php
+++ DLL.php
@@ -80,23 +80,69 @@ function mostraXML($folder){
        if ($ler != NULL){
          $xml = simplexml_load_file($ler);
          //unlink($ler);
-         echo "nome: ".$xml->link->nome."<br/>";
-         echo "Mãe: ".$xml->link->nomeMae."<br/>";
-         echo "email: ".$xml->link->email."<br/>";
-         echo "nomeSocial: ".$xml->link->nomeSocial."<br/>";
-         echo "telefone: ".$xml->link->telefone."<br/>";
-         echo "CPF: ".$xml->link->cpf."<br/>";
-         echo "RG: ".$xml->link->rg."<br/>";
-         echo "Data de Nascimento: ".$xml->link->dataNascimento."<br/>";
-         echo "sexo: ".$xml->link->sexo."<br/>";
-         echo "Tipo de Doador: ".$xml->link->tipoDoador."<br/>";
-         echo "Tipo Sanguíneo: ".$xml->link->tipoSanguineo."<br/>";
-         echo "Autorização: ".$xml->link->autorizacaoComunicacao."<br/>";
-         echo "-------------------------------------------------------------"."</br>";
-       }
+         echo"<html>
+                  <head>
+                      <title>Informações de Cadastro</title>
+                      <style>
+                          /* Estilizando a borda vermelha */
+                          .container {
+                              border: 2px solid red;
+                              padding: 20px;
+                              width: 60%px;
+                              margin: 5px;
+                          }
+                  
+                          /* Estilizando os campos */
+                          .campo {
+                              margin-bottom: 10px;
+                          }
+                      </style>
+                  </head>";
+                  echo"<body>
+                             <div class='container'>
+                               <div class='campo'>";
+                             echo "<strong> nome: </strong>".$xml->link->nome."<br/>";
+                               echo "</div>";
+                               echo"<div class='campo'>";
+                                 echo "Mãe: ".$xml->link->nomeMae."<br/>";
+                                 echo "</div>";
+                                echo" <div class='campo'>";
+                                 echo "email: ".$xml->link->email."<br/>";
+                                 echo "</div>";
+                                 echo" <div class='campo'>";
+                                 echo "nomeSocial: ".$xml->link->nomeSocial."<br/>";
+                                 echo "</div>";
+                                 echo" <div class='campo'>"; 
+                                 echo "telefone: ".$xml->link->telefone."<br/>";
+                                 echo "</div>";
+                                 echo" <div class='campo'>";
+                                 echo "CPF: ".$xml->link->cpf."<br/>";
+                                 echo "</div>";
+                                 echo" <div class='campo'>"; 
+                                 echo "RG: ".$xml->link->rg."<br/>";
+                                 echo "</div>";
+                                 echo" <div class='campo'>";
+                                 echo "Data de Nascimento: ".$xml->link->dataNascimento."<br/>";
+                               echo"</div>";
+                               echo" <div class='campo'>";
+                                 echo "sexo: ".$xml->link->sexo."<br/>";
+                                 echo "</div>";
+                                 echo" <div class='campo'>"; 
+                                 echo "Tipo de Doador: ".$xml->link->tipoDoador."<br/>";
+                                 echo "</div>";
+                                 echo" <div class='campo'>";
+                                 echo "Tipo Sanguíneo: ".$xml->link->tipoSanguineo."<br/>";
+                                 echo "</div>";
+                                 echo" <div class='campo'>";
+                                 echo "Autorização: ".$xml->link->autorizacaoComunicacao."<br/>";
+                                 echo "</div>";
+                             echo "</div>";
+                             echo "</body>";
+                             echo "</html>";
+        }
       }
    }
-   closedir($handle);
+   closedir($handle); 
  }
 }
 function mostraXMLL($folder){
