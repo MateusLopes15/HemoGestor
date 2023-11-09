3f72a92e066e5f5004a4966fe9acc4f1f0363b52
FabricioS0 <fsoliveira899@gmail.com>
2023-11-09 16:34:23 -0300

    Atualização nas cores

--- DLL.php
+++ DLL.php
@@ -124,8 +124,8 @@ function formularioAlterarDoador($caminhoArquivo){
               <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200\" />
               <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200\" />
           </head>
-          <header class=\"HemogestorBarraSuperior\">
-              <div class=\"HemogestorBarraSuperior\">
+          <header class=\"HemogestorTitulo\">
+              <div class=\"HemogestorTitulo\">
                   <span class=\"material-symbols-outlined\">
                       bloodtype
                       </span>
