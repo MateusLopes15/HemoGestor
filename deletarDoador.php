5bc205711f31d475c0d20c7b98c0e9a092c15b1d
MateusLopes15 <mateus1215.02@gmail.com>
2023-11-13 16:25:46 -0300

    redirecionamento

--- deletarDoador.php
+++ deletarDoador.php
@@ -3,13 +3,23 @@
 include "cons.php";
 include "DLL.php";
 $id = $_GET['id'];
+echo"
+
+";
 
 $status = deletarRegistro('xml/'.$id.'.xml');
 
 if($status){
     echo "Arquivo deletado com sucesso!";
+    sleep(1);
+
+    $local =$local."index.php";
+    header($local);
 }else{
     echo "Falha ao deletar registro";
+    $local =$local."index.php";
+    header($local);
 }
 echo $id;
+
 ?>
